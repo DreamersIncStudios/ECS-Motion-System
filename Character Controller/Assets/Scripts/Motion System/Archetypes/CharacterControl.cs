3bdb3f00d149d5a2070d553f77c574ea5571d3e0
The Dreamer <dreamers.inc12@gmail.com>
2019-11-06 20:07:40 -0500

    Day One: Project Setup Asset Import

---
+++ Character Controller/Assets/Scripts/Motion System/Archetypes/CharacterControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using UnityEngine.AI;
+using MotionSystem.Components;
+using IAUS.ECS.Component;
+
+
+namespace MotionSystem.Archetypes
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(CapsuleCollider))]
+    [RequireComponent(typeof(Animator))]
+
+
+    public class CharacterControl : MonoBehaviour,IConvertGameObjectToEntity
+    {
+        CapsuleCollider Col;
+        Rigidbody RB;
+        public bool AI_Control;
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            Col = this.GetComponent<CapsuleCollider>();
+            RB = this.GetComponent<Rigidbody>();
+            var control = new CharController() { CapsuleRadius = Col.radius, CapsuleCenter = Col.center, CapsuleHeight = Col.height, Mass = RB.mass };
+            dstManager.AddComponentData(entity, control);
+            if (AI_Control) {
+                var move = new Movement() { };
+                this.gameObject.AddComponent<NavMeshAgent>();
+                dstManager.AddComponentData(entity, move);
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Character Controller/Assets/Scripts/Motion System/Components/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace MotionSystem.Components
+{
+    public struct CharController: IComponentData
+    {
+        public float3 CapsuleCenter;
+        public float Mass;
+        public float CapsuleRadius;
+        public float CapsuleHeight;
+        public bool AI_Control;
+
+        public float Forward;
+        public float Turn;
+        public bool Jump;
+        public bool Crouch;
+    }
+}
