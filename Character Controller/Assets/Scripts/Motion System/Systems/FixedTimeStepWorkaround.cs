6c92fa4e57ecdff2a3fee11f73d63c3ff7f7c28b
The Dreamer <dreamers.inc12@gmail.com>
2019-11-18 21:50:40 -0500

    Day Five: Start Correcting issue with Frame Rate based jumping

---
+++ Character Controller/Assets/Scripts/Motion System/Systems/FixedTimeStepWorkaround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+namespace MotionSystem.System {
+    public class FixedTimeStepWorkaround : MonoBehaviour
+    {
+        private AnimatorUpdate AnimUpdate;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            if (AnimUpdate == null)
+            {
+                AnimUpdate = World.Active.GetOrCreateSystem<AnimatorUpdate>();
+            }
+                AnimUpdate.Update(); 
+        }
+    }
+
+}
